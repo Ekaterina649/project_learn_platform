d7478e0ff88f26f4a4ef366875016cedd54be031
Ekaterina Sarafanova <ekaterinasarayanova@gmail.com>
2026-01-20 20:21:35 +0300

    add urls

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet
+
 
 app_name = 'users'
+
+urlpatterns = [
+    # Пока пусто
+]
 
