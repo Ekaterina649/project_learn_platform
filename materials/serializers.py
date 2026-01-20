6e0150fa14114e9863aa69d7039145ea1edd2905
Ekaterina Sarafanova <ekaterinasarayanova@gmail.com>
2026-01-20 20:30:19 +0300

    fix serializer

--- materials/serializers.py
+++ materials/serializers.py
@@ -2,19 +2,22 @@ from rest_framework import serializers
 
 from materials.models import Course, Lesson
 
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = "__all__"
 
 class CourseSerializer(serializers.ModelSerializer):
     count_lessons = serializers.SerializerMethodField()
+    lessons = LessonSerializer(many=True, read_only=True)
+
 
     def get_count_lessons(self, obj):
         return obj.lessons.count()
 
     class Meta:
         model = Course
-        fields = ['id','title','preview','description','count_lessons']
+        fields = ['id','title','preview','description','count_lessons','lessons']
 
 
-class LessonSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Lesson
-        fields = "__all__"
+
