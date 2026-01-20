73a6c38673d6a3d385125c5d3eace0b796294c43
Ekaterina Sarafanova <ekaterinasarayanova@gmail.com>
2026-01-20 15:53:41 +0300

    fix

--- materials/views.py
+++ materials/views.py
@@ -6,8 +6,9 @@ from materials.serializers import CourseSerializer, LessonSerializer
 
 
 class CourseViewSet(ModelViewSet):
-    queryset =Course.objects.all()
+    queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
 
 class LessonListApiView(generics.ListAPIView):
     queryset = Lesson.objects.all()
@@ -18,13 +19,16 @@ class LessonCreateApiView(generics.CreateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
 
+
 class LessonRetrieveApiView(generics.RetrieveAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
 
+
 class LessonUpdateApiView(generics.UpdateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+
 
 class LessonDestroyApiView(generics.DestroyAPIView):
     queryset = Lesson.objects.all()
