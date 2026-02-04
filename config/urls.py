c42244e4a077c3bef1d47386e788bbff1f8dd8ad
Ekaterina Sarafanova <ekaterinasarayanova@gmail.com>
2026-02-04 20:52:27 +0300

    add stripe

--- config/urls.py
+++ config/urls.py
@@ -8,7 +8,7 @@ schema_view = get_schema_view(
     openapi.Info(
         title="API Documentation",
         default_version='v1',
-        description="Your API description",
+        description="LearnPlatform API description",
         terms_of_service="https://www.example.com/policies/terms/",
         contact=openapi.Contact(email="contact@example.com"),
         license=openapi.License(name="BSD License"),
